1251955ed143663df192fb46a41738b4609c4ea2
Patrice MALDI <patrice.maldi@gmail.com>
2021-10-31 00:21:06 +0200

    feat: add front with pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import Projects from './Projects';
+import { Route } from 'react-router-dom';
+import Sidebar from './Sidebar';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/contact" component={Contact} />
     </div>
   );
 }
