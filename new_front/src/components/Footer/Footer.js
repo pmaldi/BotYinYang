94b0ea7490e15eacae75b91d27c6e55576d470fc
Patrice MALDI <patrice.maldi@gmail.com>
2021-11-17 19:53:08 +0100

    chore: change front

---
+++ new_front/src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container, Nav, NavItem, NavLink } from "reactstrap";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Container fluid>
+        <div className="copyright">
+          © {new Date().getFullYear()} made with{" "}
+          <i className="tim-icons icon-heart-2" /> by{" "}
+          <a
+            href="https://www.creative-tim.com/?ref=bdr-user-archive-footer"
+            target="_blank"
+          >
+            Creative Tim
+          </a>{" "}
+          for a better web. And edited by{" "}
+          <a
+            href="http://pandit.fr"
+            target="_blank"
+          >
+            Patrice MALDI
+          </a>{" "}
+        </div>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ new_front/src/views/Commands.js
@@ -0,0 +1,121 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    CardText,
+    FormGroup,
+    Form,
+    Input,
+    Row,
+    Col,
+    CardTitle,
+    Table,
+} from "reactstrap";
+
+
+function Commands() {
+    return (
+        <>
+            <div className="content">
+                <Row>
+                    <Col md="12">
+                        <Card>
+                            <CardHeader>
+                                <h5 className="title">Ajouter une commande</h5>
+                            </CardHeader>
+                            <CardBody>
+                                <Form>
+                                    <Row>
+                                        <Col className="px-md-1" md="8">
+                                            <FormGroup>
+                                                <label>Commande</label>
+                                                <Input
+                                                    placeholder="!commande"
+                                                    type="text"
+                                                />
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col md="8">
+                                            <FormGroup>
+                                                <label>Message</label>
+                                                <Input
+                                                    cols="80"
+                                                    placeholder="Merci de votre confiance ! :)"
+                                                    rows="5"
+                                                    type="textarea"
+                                                />
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </CardBody>
+                            <CardFooter>
+                                <Button className="btn-fill" color="primary" type="submit">
+                                    Ajouter
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="12">
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th className="text-center">#</th>
+                                    <th>Commande</th>
+                                    <th>Message</th>
+                                    <th className="text-right">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className="text-center">3</td>
+                                    <td>!commande</td>
+                                    <td>Merci de votre confiance ! :)</td>
+                                    <td className="text-right">
+                                        <Button className="btn-icon btn-simple" color="info" size="sm">
+                                            <i className="fa fa-user"></i>
+                                        </Button>{` `}
+                                        <Button className="btn-icon btn-simple" color="success" size="sm">
+                                            <i className="fa fa-edit"></i>
+                                        </Button>{` `}
+                                        <Button className="btn-icon btn-simple" color="danger" size="sm">
+                                            <i className="fa fa-times" />
+                                        </Button>{` `}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </div>
+        </>
+    );
+}
+
+export default Commands;
